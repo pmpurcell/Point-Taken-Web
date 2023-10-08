365e004b7dfe1afb74c9e97f9e0af57287118c56
Madden <maddenpurcell@gmail.com>
2023-10-08 15:09:32 -0500

    Added navigation for both web and mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import logo from '../assets/icons/burger-icon.png';
 
 export default function Navbar() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(true);
+
+  useEffect(() => {
+    //TODO: Change this. It works smooth, but I feel there is a better to handle this in react.
+    const navigationElement = document.querySelector('.navigation');
+
+    if (hamburgerOpen) {
+      navigationElement.classList.add('slide-in');
+      navigationElement.classList.remove('slide-out');
+    } else {
+      navigationElement.classList.remove('slide-in');
+      navigationElement.classList.add('slide-out');
+    }
+  }, [hamburgerOpen]);
+
+  const toggleBurger = () => {
+    setHamburgerOpen(!hamburgerOpen)
+  }
+
   return (
-    <div id="navBar">
-      <Link className="nav-link" to="/shows">Shows</Link>
-      <Link className="nav-link" to="/music">Music</Link>
-      <Link className="nav-link" to="/about">About</Link>
-      <Link className="nav-link" to="/personnel">Personnel</Link>
+    <div>
+      <div id="navBar">
+        <Link className="app-link" to="/shows">Shows</Link>
+        <Link className="app-link" to="/music">Music</Link>
+        <Link className="app-link" to="/about">About</Link>
+        <Link className="app-link" to="/personnel">Personnel</Link>
+      </div>
+      <div className="navigation">
+        <div className="burger">
+            <Link className="app-link" to="/about">About</Link>
+          </div>
+          <div className="burger">
+            <Link className="app-link" to="/personnel">Personnel</Link>
+          </div>
+          <div className="burger">
+            <Link className="app-link" to="/music">Music</Link> 
+          </div>
+          <div className="burger">
+            <Link className="app-link" to="/shows">Shows</Link> 
+          </div>
+      </div>
+      <div className="hamburger" onClick={toggleBurger}>
+        <img src={logo} alt="Hamburger menu" />
+      </div>
     </div>
   );
 }
--- src/Initialize.js
+++ src/Initialize.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import logo from './assets/point_taken.jpg';
+import logo from './assets/transparentlogo.png';
 
 function Initialize() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img id='logo' src={logo} alt='Point Taken Logo'/>
-        <h4>Horn-spiked rock.</h4>
+      <header className="app-header">
+        <section className="curved">
+          <div className="cover-box">
+            <img id='logo' src={logo} alt='Point Taken Logo'/>
+          </div>
+          <h4>Horn-spiked rock from Nashville, Tennessee</h4>
+        </section>
         <Navbar/>
       </header>
     </div>
