a5fcef2f98305a0b0f91e7863bf83872297340f1
Madden <maddenpurcell@gmail.com>
2023-10-12 19:08:28 -0500

    Positioned music player

--- src/Components/TrackCard.js
+++ src/Components/TrackCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function TrackCard({ imageString, trackTitle, audioString }) {
   return (
@@ -7,12 +8,8 @@ export default function TrackCard({ imageString, trackTitle, audioString }) {
       <img className="track-image" src={imageString} alt="{trackTitle} cover" />
       <div className="track-information">
         <h3 className="track-title">{trackTitle}</h3>
-        <p>Lyrics</p>
-      </div>
-      <div className="track-audio">
-        <audio  controls>
-          <source type="audio/wav" src={audioString} />
-        </audio>
+        <p onClick={() => {console.warn('click')}}>Lyrics</p>
+      <ReactAudioPlayer className="track-audio" src={audioString} controls />
       </div>
     </div>
   );
