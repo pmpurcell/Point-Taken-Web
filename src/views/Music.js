d9e877f29b463e3b543cce7276b6fdd11273b436
Madden <maddenpurcell@gmail.com>
2023-10-12 20:57:43 -0500

    Properly named music files

--- src/views/Music.js
+++ src/views/Music.js
@@ -2,8 +2,8 @@ import React from 'react'
 import TrackCard from '../Components/TrackCard'
 import stranger from '../assets/1A.jpg'
 import car from '../assets/front_2.jpg'
-import carAudio from '../assets/audio/Stranger In The House F8M3.wav'
-import strangerAudio from '../assets/audio/01_Car_Door.wav'
+import strangerAudio from '../assets/audio/Stranger In The House F8M3.wav'
+import carAudio from '../assets/audio/01_Car_Door.wav'
 import {default as strangerLyrics} from "../assets/lyrics/Stranger.txt";
 import {default as carDoorLyrics} from "../assets/lyrics/Car_Door.txt";
 
